8293a323c41f86577a7ebcb22c88fa34e290c00a
alesya-kuptsova <a5484@icloud.com>
2022-05-21 12:59:14 +0300

    Lab7_lab8
    Added Lab7 and Lab8

---
+++ TestProj_2022/TestProj-main/TestProj/TestProj/engine.js
@@ -0,0 +1,20 @@
+﻿//If you want to use the file:
+var t = new Array(9);
+
+function ai() {
+    //add code here, if you want to do game with ai (optional!)
+}
+
+function checkEnd() {
+    // add code
+ return true;
+}
+
+function move(id, role) {
+    // add code
+}
+
+function reset() {
+    alert("Game over!");
+    location.reload();
+}
